f48ed0bb0685fa25f6bd97bdf34315a8ba7c4c6f
t3rmit4nk <official.simone.rizzo@gmail.com>
2020-12-23 11:42:50 +0100

    Merge branch 'main' into sim
    # Conflicts:
    #	src/PerceptronProva/main.py
    #	src/__pycache__/abGridSearchCV.cpython-37.pyc
    #	src/__pycache__/loadData.cpython-37.pyc
    #	src/__pycache__/perceptron.cpython-37.pyc
    #	src/loadData.py
    #	src/main.py
    #	src/perceptron.py

--- src/abGridSearchCV.py
+++ src/abGridSearchCV.py
@@ -1,17 +1,12 @@
-#
-#  Created by Abhinav Dwivedi on 30/01/2019.
-#  Copyright © 2019 Abhinav Dwivedi. All rights reserved.
-#
-
 from sklearn.model_selection import train_test_split
 from itertools import product
-from src.perceptron import perceptron
+from network import NeuralNetwork
 import numpy as np
 
 
 def abGridSearchCV(defaultParams, paramGrid, features, labels, validationSplit, winnerCriteria, log=True, topn=5, logToBeReturned=[]):
     """
-    Returns the log(if log=True) and best parameters for the 'perceptron' model by evaluating the model on all the combinations of given parameters.
+    Returns the log(if log=True) and best parameters for the 'NeuralNetwork' model by evaluating the model on all the combinations of given parameters.
     Usage:
 
     logOfGridSearch, topParams=abGridSearchCV(defaultParams, paramGrid, features, labels, validationSplit, winnerCriteria, topn)
@@ -25,41 +20,44 @@ def abGridSearchCV(defaultParams, paramGrid, features, labels, validationSplit, 
     log:if True, returns the log of all the combinations,
     topn:how many winners
     """
-    
-    assert winnerCriteria in ["meanLosses", "meanTrainingLoss", "meanValidationLoss"], "This function currently doesn't support the winner criteria provided. Please make sure it's 'meanLosses' or 'meanTrainingLoss', or 'meanValidationLoss'"
-    
-    listParams=list(paramGrid.values())
-    winners=[]
-    allCombinations=list(product(*listParams))
-    trainData_, validationData_, trainLabels_, validationLabels_=train_test_split(features, labels, test_size=validationSplit)    
+
+    assert winnerCriteria in ["meanLosses", "meanTrainingLoss",
+                              "meanValidationLoss"], "This function currently doesn't support the winner criteria provided. Please make sure it's 'meanLosses' or 'meanTrainingLoss', or 'meanValidationLoss'"
+
+    listParams = list(paramGrid.values())
+    winners = []
+    allCombinations = list(product(*listParams))
+    trainData_, validationData_, trainLabels_, validationLabels_ = train_test_split(
+        features, labels, test_size=validationSplit)
     for index, val in enumerate(allCombinations):
-        print("                        {}/{}".format(index, len(allCombinations)), end="\r")
-        param={}
+        print("                        {}/{}".format(index,
+                                                     len(allCombinations)), end="\r")
+        param = {}
         for index_ in range(len(paramGrid)):
-            param[list(paramGrid.keys())[index_]]=val[index_]
-        model=perceptron(**defaultParams)
-        model.set_params(**param)
-        model.fit(trainData_, trainLabels_, validationData_, validationLabels_, earlyStoppingLog=False, comingFromGridSearch=True)
-        meanLoss=np.mean(model.losses)
-        meanValidationLoss=np.mean(model.validationLosses)
-        if model.newEpochNotification:
-            param['epochs']=model.bestEpoch
-        tempLog={
-            'params': param, 
-            'meanTrainingLoss':meanLoss, 
-            'meanValidationLoss':meanValidationLoss,
-            'meanLosses':(meanLoss+meanValidationLoss)/2
+            param[list(paramGrid.keys())[index_]] = val[index_]
+        model = NeuralNetwork(**param)
+        model.fit(trainData_, trainLabels_, validationData_, validationLabels_,
+                  early_stoppingLog=False, comingFromGridSearch=True)
+        meanLoss = np.mean(model.train_losses)
+        meanValidationLoss = np.mean(model.valid_losses)
+        # if model.newEpochNotification:
+        # param['epochs']=model.bestEpoch
+        tempLog = {
+            'params': param,
+            'meanTrainingLoss': meanLoss,
+            'meanValidationLoss': meanValidationLoss,
+            'meanLosses': (meanLoss+meanValidationLoss)/2
         }
         if log:
             logToBeReturned.append(tempLog)
-            
-        if len(winners)<topn:
+
+        if len(winners) < topn:
             winners.append(tempLog)
         else:
             winners = sorted(winners, key=lambda k: k[winnerCriteria])
-            if tempLog[winnerCriteria]<winners[-1][winnerCriteria]:
-                winners[-1]=tempLog
-        
+            if tempLog[winnerCriteria] < winners[-1][winnerCriteria]:
+                winners[-1] = tempLog
+
     if log:
         return logToBeReturned, winners
     else:
--- src/main.py
+++ src/main.py
@@ -1,62 +1,58 @@
 import numpy
 
-from src.abGridSearchCV import abGridSearchCV
-from src.loadData import loadMonk
-from src.perceptron import perceptron
+from abGridSearchCV import abGridSearchCV
+from load_data import load_monk
+from network import NeuralNetwork
 import matplotlib.pyplot as plt
 
-trainData, trainLabels = loadMonk(3, 'train', encodeLabel=False)
-
-defaultParameters={
-    'hiddenUnits':3,
-    'randomSeed':0,
-    'activation':'sigm',
-    'epochs':500,
-    'ETA':0.4,
-    'LAMBDA':0.0,
-    'loss':"MEE",
-    'ALPHA':0.9,
-    'weightInitialization':'xav',
-    'regression':False,
-    'earlyStopping':True,
-    'tolerance':1e-3,
-    'patience':20
+defaultParameters = {
+    'hidden_units': 3,
+    'activation': 'sigm',
+    'epochs': 500,
+    'ETA': 0.4,
+    'LAMBDA': 0.0,
+    'loss': "MEE",
+    'ALPHA': 0.9,
+    'weightInitialization': 'xav',
+    'regression': False,
+    'earlyStopping': True,
+    'tolerance': 1e-3,
+    'patience': 20
 }
 
-parameterGridForModelSelection={
-#     'randomSeed':[0, 20],
-    'hiddenUnits':[4, 16],
-    'activation':['sigm', 'relu', 'tanh'],
-    'ETA':[0.1, 0.2, 0.3, 0.5, 0.7,0.9],
-    'LAMBDA':[0.001,0.01],
-    'ALPHA':[0.2, 0.5, 0.7, 0.9]
+parameterGridForModelSelection = {
+    'hidden_units': [4, 16],
+    'activation': ['sigm', 'relu', 'tanh'],
+    'ETA': [0.1, 0.2, 0.3, 0.5, 0.7, 0.9],
+    'LAMBDA': [0.001, 0.01],
+    'ALPHA': [0.2, 0.5, 0.7, 0.9]
 }
 # top5BestParams = abGridSearchCV(defaultParameters, parameterGridForModelSelection, trainData, trainLabels, winnerCriteria="meanLosses", validationSplit=0.3, log=False, topn=5)
-# bestParams = top5BestParams[0]['params']
-"""bestParams = {
- 'ALPHA': 0.5,
- 'ETA': 0.3,
- 'LAMBDA': 0.001,
- 'activation': 'sigm',
- 'epochs': 413,
- 'hiddenUnits': 4
-}"""
-bestParams = { #for monk 3 0.9722
- 'ALPHA': 0.8,
- 'ETA': 0.1,
- 'LAMBDA': 0.1,
- 'activation': 'sigm',
- 'epochs': 400,
- 'hiddenUnits': 4
+# bestParams = top5BestParams[0]
+# print(bestParams)
+bestParams = {
+    'ALPHA': 0.8,
+    'ETA': 0.1,
+    'LAMBDA': 0.001,
+    'activation': 'sigm',
+    'epochs': 413,
+    'hidden_units': 0
 }
 defaultParameters['earlyStopping'] = False
-testData, testLabels = loadMonk(3, 'test', encodeLabel=False)
-x = perceptron(**defaultParameters)
-x.set_params(**bestParams)
-x.fit(trainData, trainLabels)
-testResults, testAccuracy = x.predict(testData, testLabels, acc_=True)
-print("accuracy")
-print(testAccuracy)
-plt.plot(numpy.array(x.losses))
-plt.plot(numpy.array(x.accuracies))
-plt.show()
+
+total = 0
+n = 100
+monk = 1
+trainData, trainLabels = load_monk(monk, 'train', encodeLabel=False)
+testData, testLabels = load_monk(monk, 'test', encodeLabel=False)
+for i in range(n):
+    x = NeuralNetwork(**bestParams)
+    x.fit(trainData, trainLabels, )
+    testResults, testAccuracy = x.predict(testData, testLabels, acc_=True)
+    # plt.plot(numpy.array(x.losses))
+    # plt.plot(numpy.array(x.accuracies))
+    # plt.show()
+    print("accuracy test")
+    print(testAccuracy)
+    total += testAccuracy
+print(f"final {total/n}")
