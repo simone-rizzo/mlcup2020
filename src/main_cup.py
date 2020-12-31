88beaab31c2a278270daf751984b073c538ee7cf
t3rmit4nk <official.simone.rizzo@gmail.com>
2020-12-31 20:25:35 +0100

    giocato con hyperparam se ETA troppo alto esplodono le funzioni

--- src/main_cup.py
+++ src/main_cup.py
@@ -2,16 +2,18 @@ from src.grid_search import model_selection, model_assessment
 from src.load_data import load_monk, load_cup
 from src.network import DeepNeuralNetwork
 import matplotlib.pyplot as plt
+import numpy as np
 
 params = {
-    'layer_sizes': [10, 10, 10, 2],
+    'layer_sizes': [10, 100, 50, 2],
     'act_hidden': 'relu',
     'act_out': 'iden',
-    'ETA': 0.5,
-    'LAMBDA': 0.1,
-    'ALPHA': 0.9,
-    'WEIGHT_INI': 'xav',
-    'regression': True
+    'ETA': 0.001,
+    'LAMBDA': 0.01,
+    'ALPHA': 0.6,
+    'WEIGHT_INI': 'he',
+    'regression': True,
+    'epochs': 500
 }
 
 filename = "../data/cup/ML-CUP20-TR.csv"
@@ -20,4 +22,5 @@ nn = DeepNeuralNetwork(**params)
 nn.fit(tr_data, tr_label)
 plt.plot(nn.train_losses)
 plt.show()
+print(np.amin(nn.train_losses))
 # model_assessment(params, tr_data, tr_label, tr_data, tr_label)
